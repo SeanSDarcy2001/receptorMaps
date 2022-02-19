49316b9b09100c62408e17cfa0b47a34c9e6e69a
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-02-19 15:32:14 -0500

    added init

---
+++ cpcrCode/__init__.py
@@ -0,0 +1,5 @@
+from .getData import getData
+from .saveData import saveData
+from .generateDictionary import generateDictionary
+
+__all__ = ["getData", "saveData", "generateDictionary"]
--- cpcrCode/getData.py
+++ cpcrCode/getData.py
@@ -8,12 +8,15 @@ class getData:
         self.MNImaps = []
         self.keys = []
         self.path = path
+        print("we get here")
         fileList = Path(path).glob('**/*.gz')
         for file in fileList:
+            print(file)
             self.MNImaps.append(file)
             self.keys.append(str(file))
 
     def getMaps(self) :
+        print("we called get maps")
         return self.MNImaps
 
     def getKeys(self) :
