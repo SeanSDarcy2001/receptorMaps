54a9d32b94f9fe9f8adfc23a9552cff38624a02b
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-03-30 12:33:04 -0400

    comparisonScript

---
+++ comparisonScript.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 11:31:29 2022
+
+@author: bwinsto2
+"""
+import sys
+sys.path.append('/usr/local/connectome_harmonic_core/connectome_harmonic_core')
+
+import input_output as inout
+import utility_functions as uts
+import test_retest_fxns as t_rt
+import numpy as np
+from pathlib import Path
+import json
+from cpcrCode import saveData
+
+
+#mask every receptor map. you can use uts.mask_medial_wall_vecs (which is in utility_functions insdie the CHAP repo)
+    
+#for each subject/session, there will be vecs.npy in
+#/data/hcp_test_retest/derivatives/chap/ = chap directory
+#compare each receptor map with each harmonic (each sub/session has 100)
+
+subs = inout.get_subs('/data/hcp_test_retest/derivatives/chap/')
+#mask = np.load('/data2/Brian/connectome_harmonics/mask.npy')
+maps = json.load('/outputs/fsLR32k_beliveau2017maps')
+
+#mask receptor map medial wall
+maskedMaps = {}
+for rec in ['5-HT1A', '5-HT1B', '5-HT2A', '5-HT4', '5-HTT']: 
+    maskedMaps[rec] = uts.mask_medial_wall_vecs(maps[rec])
+
+#get subject vecs and generate comparison
+#comparisonDictionary is a nested structure queried by subject, receptor, harmonic
+comparisonDictionary = {}
+for subjects in subs:
+    comparisonDictionary[subjects] = {}
+    vectors = np.load('/vecs.npy')
+    for receptor in maskedMaps :
+        comparisonDictionary[subjects][receptor] = {}
+        for vecs in vectors:
+            comparisonDictionary[subjects][receptor][vecs] = np.absolute(receptor - vecs)
+
+output_dir = Path("outputs").resolve()
+if not output_dir.exists():
+        output_dir.mkdir()
+
+saver = saveData("comparisons")
+saver.save(comparisonDictionary, output_dir)
+
+#ignore below
+
+#chap_dir = '/data/HCP_Raw/derivatives/chap'
+#subs = inout.get_subs(chap_dir)
+#for sub in subs:
+    #ses_dir = f'{chap_dir}/sub-{sub}/'
+    #if os.path.exists(f'{ses_dir}/connectome.npz'):
+        #os.remove(f'{ses_dir}/connectome.npz')
