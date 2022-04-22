7c1dd5f6adb5ce843cf417df7ecb5940243c8f32
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-04-22 11:46:05 -0400

    seeing if 1 vector has 64kvals

--- comparisonScript.py
+++ comparisonScript.py
@@ -52,7 +52,7 @@ def main() :
             comparisonDictionary[subjects][receptor] = {}
             densities = maskedMaps[receptor]
             for vecs in vectors:
-                print(vecs.shape)
+                print(vecs[0].shape)
                 print(densities.shape)
                 comparisonDictionary[subjects][receptor][vecs] = np.absolute(np.subtract(densities, vecs))
 
