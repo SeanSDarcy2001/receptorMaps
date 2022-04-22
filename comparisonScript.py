39174a29e207012f0537ec9edc0f2d1cb4c7d8ed
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-04-22 10:36:31 -0400

    debugging

--- comparisonScript.py
+++ comparisonScript.py
@@ -33,8 +33,10 @@ def main() :
 
 #mask receptor map medial wall
     maskedMaps = {}
+    mask = np.load('inputs/hcp_mask.npy')
+    print(mask)
     for rec in ['5-HT1A', '5-HT1B', '5-HT2A', '5-HT4', '5-HTT']: 
-        maskedMaps[rec] = uts.mask_medial_wall_vecs(maps[rec], np.load('inputs/hcp_mask.npy'))
+        maskedMaps[rec] = uts.mask_medial_wall_vecs(maps[rec], mask)
 
 #get subject vecs and generate comparison
 #comparisonDictionary is a nested structure queried by subject, receptor, harmonic
