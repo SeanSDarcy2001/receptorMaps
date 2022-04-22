bdeb043148e2c3776e18627739e735189ffeee25
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-04-22 12:16:54 -0400

    deb

--- comparisonScript.py
+++ comparisonScript.py
@@ -51,8 +51,9 @@ def main() :
         for receptor in maskedMaps :
             comparisonDictionary[subjects][receptor] = {}
             densities = maskedMaps[receptor]
+            print(vectors.shape)
             for vecs in vectors:
-                print(vecs[0].shape)
+                #print(vecs.shape)
                 print(densities.shape)
                 comparisonDictionary[subjects][receptor][vecs] = np.absolute(np.subtract(densities, vecs))
 
