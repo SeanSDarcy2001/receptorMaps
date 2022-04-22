df371fcba09143fca59a9e6a4552214c9bfc7a4a
SeanSDarcy2001 <sdarcy2@jhu.edu>
2022-04-22 10:17:45 -0400

    path bug fix

--- comparisonScript.py
+++ comparisonScript.py
@@ -28,7 +28,7 @@ def main() :
     subs = inout.get_subs('/data/hcp_test_retest/derivatives/chap/')
 #mask = np.load('/data2/Brian/connectome_harmonics/mask.npy')
 
-    mapsFile = open('/outputs/fsLR32k_beliveau2017maps')
+    mapsFile = open('outputs/fsLR32k_beliveau2017maps')
     maps = json.load(mapsFile)
 
 #mask receptor map medial wall
